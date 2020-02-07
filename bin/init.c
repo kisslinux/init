a5056e006615689ce0a82c248774cb5b5d358d9b
Dylan Araps <dylan.araps@gmail.com>
2020-02-07 14:48:05 +0200

    bin: Added init.c

---
+++ bin/init.c
@@ -0,0 +1,22 @@
+#define _XOPEN_SOURCE 700
+#include <signal.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    sigset_t set;
+    int status;
+
+    if (getpid() != 1) return 1;
+
+    sigfillset(&set);
+    sigprocmask(SIG_BLOCK, &set, 0);
+
+    if (fork()) for (;;) wait(&status);
+
+    sigprocmask(SIG_UNBLOCK, &set, 0);
+
+    setsid();
+    setpgid(0, 0);
+    return execve("/lib/init/rc.boot", (char *[]){0}, (char *[]){0});
+}
